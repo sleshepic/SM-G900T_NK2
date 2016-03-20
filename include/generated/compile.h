fd4b826b7a9f88994b16d32f69b29fc26d193b2e
sleshepic <ian.slezra@gmail.com>
2016-03-20 11:45:12 -0400

    retry - add bfq fiops and sio sched

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Mar 20 10:32:12 EDT 2016"
-#define LINUX_COMPILE_BY "ian"
-#define LINUX_COMPILE_HOST "ian-Aspire-7750G"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
